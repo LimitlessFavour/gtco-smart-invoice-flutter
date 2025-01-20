d60e555ec9e507a770089829e2c6e752ac117f5f
limitlessfavour <oketolulope3@gmail.com>
2025-01-20 10:19:54 +0100

    fix: product create web bug

--- lib/widgets/common/slide_panel.dart
+++ lib/widgets/common/slide_panel.dart
@@ -14,28 +14,50 @@ class SlidePanel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedPositioned(
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
-      right: isOpen ? 0 : -800,
-      top: 0,
-      bottom: 0,
-      width: 800,
-      child: Material(
-        elevation: 16,
-        type: MaterialType.transparency,
-        child: Container(
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(8),
-              bottomLeft: Radius.circular(8),
+    return Stack(
+      children: [
+        // Backdrop for clicking outside
+        if (isOpen)
+          Positioned.fill(
+            child: GestureDetector(
+              onTap: onClose,
+              child: Container(
+                color: Colors.black.withOpacity(0.3),
+              ),
             ),
-            // border: Border.all(color: const Color(0xFFC6C1C6)),
           ),
-          child: child,
+        AnimatedPositioned(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          right: isOpen ? 0 : -800,
+          top: 0,
+          bottom: 0,
+          width: 800,
+          child: GestureDetector(
+            onHorizontalDragEnd: (details) {
+              if (details.primaryVelocity != null &&
+                  details.primaryVelocity! > 0) {
+                onClose();
+              }
+            },
+            child: Material(
+              elevation: 16,
+              type: MaterialType.transparency,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(8),
+                    bottomLeft: Radius.circular(8),
+                  ),
+                  // border: Border.all(color: const Color(0xFFC6C1C6)),
+                ),
+                child: child,
+              ),
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
